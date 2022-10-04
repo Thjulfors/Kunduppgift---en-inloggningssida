78786f6e11d591398f91f932033394941aec5ede
Thjulfors <112393579+Thjulfors@users.noreply.github.com>
2022-10-04 14:04:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-//Hämtar elementen med de specifika iden och sparar dem i variabler.
-//för att sedan återanvändas i funktionerna nedan.
+
 const inpKey = document.getElementById("inpKey");
 const inpValue = document.getElementById("inpValue");
 const btnInsert = document.getElementById("btnInsert");
 
-//Använder variabel med metod som skickar in data i min funktion.
-// Där deklareras 2 nya variabler.
+
 btnInsert.onclick = function () {
   const key = inpKey.value;
   const value = inpValue.value;
 
-  //If sats för att kontrollera om inloggningsuppgifterna stämmer.
-  //Gör de det sparas användarens uppgifter i localStorage och man loggas in.
-  // Meddelande för att välkomna användaren.
-  //Knappen byter text till "Logga ut"
-  // Om inte - användaren får ett meddelande om att försöka igen.
+  
   if (key == "Sara" && value == "qwe123") {
     localStorage.setItem(key, value);
     alert("Välkommen, du är nu inloggad");
     document.getElementById("btnInsert").innerHTML = "Logga ut";
-    // Funktion för att styra utloggningsfuntionen. Samma princip som ovan
-    // It-sats som är nestad i en if-sats.
-    //location.reload för att ladda om sidan.
+    
     btnInsert.onclick = function () {
       if (key == "Sara" && value == "qwe123") {
         location.reload(alert("Du är nu utloggad"));
